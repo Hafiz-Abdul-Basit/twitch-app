c469278c113347f111660cc677088c4b06b5f4e1
Hafiz-Abdul-Basit <110740437+Hafiz-Abdul-Basit@users.noreply.github.com>
2023-01-29 08:57:24 +0500

    my-first-commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "robohash.org",
+      "static-cdn.jtvnw.net",
+      "img.icons8.com",
+      "static.vecteezy.com",
+      "cdna.artstation.com",
+      "img2.bc.game",
+    ],
+  },
+};
